8cf0433654b296d7a0d02ca1b1e2f864e24e9e0f
Antor Ahmed <32675798+AntorOfficial@users.noreply.github.com>
2020-06-11 18:49:01 +0600

    Add files via upload

---
+++ examples/HelloWorld/HelloWorld.ino
@@ -0,0 +1,17 @@
+#include <LiquidCrystalSerial.h>
+
+#define SH_CP 12
+#define DS    11
+#define ST_CP 10
+
+LiquidCrystalSerial lcd(SH_CP, DS, ST_CP);
+
+void setup() {
+  lcd.begin(16, 2, SERIAL);
+  lcd.print("hello, world!");
+}
+
+void loop() {
+  lcd.setCursor(0, 1);
+  lcd.print(millis() / 1000);
+}
